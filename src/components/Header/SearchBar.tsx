888cb8a1bc0b03dc85a82c6643f584b254d44485
Ichsan Zulfikar <ichsanzulfikar2141@gmail.com>
2023-06-10 03:55:54 +0700

    change some properties

--- src/components/Header/SearchBar.tsx
+++ src/components/Header/SearchBar.tsx
@@ -11,8 +11,8 @@ export function SearchBar(props: SearchBarProps){
     }
 
     return(
-        <div className="my-4 mx-auto w-[300px] font-semibold">
-            <input type="text" className="w-full rounded-xl text-lg py-2 px-2" placeholder="Search for Photos..." value={props.queryString} onChange={handleFormEvent}/>
+        <div className="my-4 mx-auto w-[600px] font-bold">
+            <input type="text" className="w-full border border-black rounded-xl text-lg py-2 px-2" placeholder="Search for Photos..." value={props.queryString} onChange={handleFormEvent}/>
         </div>
     )
 }
